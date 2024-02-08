ce3f626f06ee5cd9e525a83abe3207e9f1c0836c
Homer Rivera <hmrwebdev@gmail.com>
2024-02-08 01:29:25 -0500

    changed the array object key value pair inside all_product.js from category:kid to category:kids to match the category prop inside of the App.jsx.

--- src/pages/shopCategory/ShopCategory.jsx
+++ src/pages/shopCategory/ShopCategory.jsx
@@ -1,6 +1,8 @@
 import './shopCategory.scss';
 import { ShopContext } from '../../context/ShopContext';
 import {useContext}  from 'react';
+import dropdown_icon from '../../assets/dropdown_icon.png';
+import Items from '../../components/items/Items';
 
 const ShopCategory = (props) => {
     const {all_product} = useContext(ShopContext);
@@ -8,6 +10,24 @@ const ShopCategory = (props) => {
     return ( 
         <div className="shopCategory">
             <img src={props.banner} alt="" />
+            <div className="shopCategory-indexSort">
+                <p>
+                    <span>Showing 1-12</span> out of 36 Products
+                </p>
+                <div className="shopCategory-sort">
+                    Sort by <img src={dropdown_icon} alt="" />
+                </div>
+            </div>
+            <div className="shopCategory-products">
+                {all_product.map((item, i) => {
+                    if (props.category===item.category) {
+                        return <Items key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
+                    }
+                    else {
+                        return null;
+                    }
+                })}
+            </div>
         </div>
      );
 }
